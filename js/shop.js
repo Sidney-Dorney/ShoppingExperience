94069a61e76c118a4c2cd05dc062d0e984b4b638
Sidney Dorney <sidney.dorney314@gmail.com>
2024-12-03 12:16:54 -0700

    fix card div in shop.js

--- js/shop.js
+++ js/shop.js
@@ -2,13 +2,13 @@ const cardContainer = document.querySelector("#cardContainer");
 function addProducts(){
     products.forEach(individualCard => {
         cardContainer.innerHTML += 
-        `<div class="card" id= "${individualCard.id}">
+        `<div class="card" id= "cardNumber${individualCard.id}">
            <img src= "${individualCard.image}" alt= "${individualCard.description}">
             <div class="cardText">
                 <h4>${individualCard.name}</h4>
                 <p>${individualCard.description}</p>
                 <p>&dollar; ${individualCard.price}</p>
-                <button class="cartButton" id="cardNumber${individualCard.id}">Add to Cart</button>
+                <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
             </div>
         </div>`;
     });
